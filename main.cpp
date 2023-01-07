059d4cefcc169f7ebabba7660e672c966a2c71c7
comradevkar <ev.karasev1971@yandex.ru>
2023-01-07 23:27:59 +0300

    added second and third line

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@ public class Main{
 
 	public static void main(String[] args){
 		System.out.println("First line");
+		System.out.println("Second line");
+		System.out.println("Third line");
+
+
 	}
 
 }
